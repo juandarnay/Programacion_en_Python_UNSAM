7528fe8efd33b7ef37e76bf0cfba8ee908bcfa7e
juandarnay <66964391+juandarnay@users.noreply.github.com>
2021-04-02 10:48:50 -0300

    Delete Clase 02 directory

--- Clase 02/2.7 - buscar_precios.py
+++
@@ -1,8 +0,0 @@
-import csv
-f = open(r'C:\Dropbox\Ciencia de datos\Programacion en Python UNSAM\Notas\01_ejercicios_python\Data\camion.csv')
-rows = csv.reader(f)
-headers = next(rows)
-headers
-for row in rows:
-        print(row)
-f.close()
